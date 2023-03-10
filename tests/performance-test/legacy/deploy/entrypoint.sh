6fd9b82a3ba97552ca8f2ebe9efc7257dcf3378a
Leif Madsen <lmadsen@redhat.com>
2023-03-10 14:26:37 -0500

    Remove all existing files in prep for new build

--- tests/performance-test/legacy/deploy/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-/usr/sbin/collectd -C /tmp/minimal-collectd.conf -f 2>&1 | tee /tmp/collectd_output
-
-sleep 5
--- tests/promxy/promxy-launcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-oc delete \
-   deployment/promxy \
-   configmap/promxy-config
-
-oc create -f promxy-manifests.yaml
-
-oc rollout status deployment/promxy
